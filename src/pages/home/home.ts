ec4bb23ab48109479b8c5634131ef41b23ffedfe
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2019-03-19 08:53:46 -0300

    Comecando a implementar a autenticacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage, MenuController } from 'ionic-angular';
 import { credenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -14,7 +15,10 @@ export class HomePage {
     senha: ""
   }
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController, 
+  public auth: AuthService) {
 
   }
 
@@ -27,8 +31,14 @@ export class HomePage {
   }
   
   login() {
-    console.log(this.creds);
+    this.auth.authenticate(this.creds)
+    .subscribe(response =>{
+      console.log(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('CategoriasPage');
+    },
+    erro => {})
+
     // this.navCtrl.push('CategoriasPage'); // PHI: esse método push() ele empilha a página colocando a setinha de voltar
-    this.navCtrl.setRoot('CategoriasPage'); // PHI: esse método setRoot() ele entra na página e demonstra o Botão do Menu
+    //this.navCtrl.setRoot('CategoriasPage'); // PHI: esse método setRoot() ele entra na página e demonstra o Botão do Menu
   }
 }
